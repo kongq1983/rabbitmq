580584a6e5c467093197bc70b667f90111f18a8a
king <kongq1983@qq.com>
2018-06-10 00:04:22 +0800

    first commit

---
+++ src/main/java/com/kq/rabbitmq/demo/RabbitConsumer.java
@@ -0,0 +1,41 @@
+package com.kq.rabbitmq.demo;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class RabbitConsumer {
+
+    public static void main(String[] args) throws Exception{
+        Address[] addresses = new Address[]{new Address(RabbitProducer.IP_ADDRESS,RabbitProducer.PORT)};
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername(RabbitProducer.USERNAME);
+        factory.setPassword(RabbitProducer.PASSWORD);
+
+        Connection connection = factory.newConnection(addresses);
+        final Channel channel = connection.createChannel();
+        channel.basicQos(64); //设置客户端最多接收未被ack的消息的个数
+
+        Consumer consumer = new DefaultConsumer(channel) {
+          public void handleDelivery(String consumerTag,Envelope envelope,AMQP.BasicProperties properties,byte[] body) throws IOException {
+              System.out.println("reveive message : "+new String(body));
+
+              try{
+                  TimeUnit.SECONDS.sleep(1);
+              }catch (InterruptedException e1){
+                    e1.printStackTrace();
+              }
+              channel.basicAck(envelope.getDeliveryTag(),false);
+          }
+        };
+
+        channel.basicConsume(RabbitProducer.QUEUE_NAME,consumer);
+        TimeUnit.SECONDS.sleep(5);
+        channel.close();
+        connection.close();
+
+    }
+
+}
---
+++ src/main/java/com/kq/rabbitmq/demo/RabbitProducer.java
@@ -0,0 +1,44 @@
+package com.kq.rabbitmq.demo;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+public class RabbitProducer {
+
+    public static final String EXCHANGE_NAME = "exchange_demo";
+    public static final String ROUTING_KEY = "routingkey_demo";
+    public static final String QUEUE_NAME = "queue_demo";
+    public static final String IP_ADDRESS = "192.168.3.200";
+    public static final String USERNAME = "admin";
+    public static final String PASSWORD = "123456";
+    public static final int PORT = 5672;
+
+
+    public static void main(String[] args) throws Exception{
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(IP_ADDRESS);
+        factory.setPort(PORT);
+        factory.setUsername(USERNAME);
+        factory.setPassword(PASSWORD);
+
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        //创建一个type="direct"，持久化的、非自动删除的交换器
+        channel.exchangeDeclare(EXCHANGE_NAME,"direct",true,false,null);
+        //创建一个持久化、非排他的、非自动删除的队列
+        channel.queueDeclare(QUEUE_NAME,true,false,false,null);
+        //将交换器与队列通过路由键绑定
+        channel.queueBind(QUEUE_NAME,EXCHANGE_NAME,ROUTING_KEY);
+        //发送一条持久化的消息
+        String message = "Hello World!";
+        channel.basicPublish(EXCHANGE_NAME,ROUTING_KEY,MessageProperties.PERSISTENT_TEXT_PLAIN,message.getBytes());
+        //关闭资源
+        channel.close();
+        connection.close();
+
+    }
+
+
+}
